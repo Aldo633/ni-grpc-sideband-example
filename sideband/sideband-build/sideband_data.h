d0554cb1d4c5a70ea025e5fcb8c12bc12e00fc2c
Aldo <aldo.hernandez@ni.com>
2025-10-09 00:47:16 +0000

    Added grpc built library

---
+++ sideband/sideband-build/sideband_data.h
@@ -0,0 +1,82 @@
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+#pragma once
+#include <cstdint>
+#include <atomic>
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+#if defined(_WIN32)
+    #define SIDEBAND_C_CONV __cdecl
+    #if defined(_BUILDING_GRPC_SIDEBAND)
+        #define SIDEBAND_IMPORT_EXPORT __declspec(dllexport)
+    #else
+        #define SIDEBAND_IMPORT_EXPORT __declspec(dllimport)
+    #endif
+#else
+    #define SIDEBAND_C_CONV
+    #if defined(_BUILDING_GRPC_SIDEBAND)
+        #define SIDEBAND_IMPORT_EXPORT __attribute__ ((section (".export")))
+    #else
+        #define SIDEBAND_IMPORT_EXPORT
+    #endif
+#endif
+#define  _SIDEBAND_FUNC SIDEBAND_IMPORT_EXPORT SIDEBAND_C_CONV
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+enum class SidebandStrategy
+{
+  UNKNOWN = 0,
+  GRPC = 1,
+  SHARED_MEMORY = 2,
+  DOUBLE_BUFFERED_SHARED_MEMORY = 3,
+  SOCKETS = 4,
+  SOCKETS_LOW_LATENCY = 5,
+  HYPERVISOR_SOCKETS = 6,
+  RDMA = 7,
+  RDMA_LOW_LATENCY = 8
+};
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+int32_t _SIDEBAND_FUNC InitOwnerSidebandData(::SidebandStrategy strategy, int64_t bufferSize, char* out_sideband_id);
+int32_t _SIDEBAND_FUNC GetOwnerSidebandDataToken(const char* usageId, int64_t* out_tokenId);
+int32_t _SIDEBAND_FUNC InitClientSidebandData(const char* sidebandServiceUrl, ::SidebandStrategy strategy, const char* usageId, int bufferSize, int64_t* out_tokenId);
+int32_t _SIDEBAND_FUNC WriteSidebandData(int64_t dataToken, uint8_t* bytes, int64_t bytecount);
+int32_t _SIDEBAND_FUNC ReadSidebandData(int64_t dataToken, uint8_t* bytes, int64_t bufferSize, int64_t* numBytesRead);
+int32_t _SIDEBAND_FUNC CloseSidebandData(int64_t dataToken);
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+int32_t _SIDEBAND_FUNC RunSidebandSocketsAccept(const char* address, int port, std::atomic<bool>& stop_flag);
+int32_t _SIDEBAND_FUNC AcceptSidebandRdmaSendRequests();
+int32_t _SIDEBAND_FUNC AcceptSidebandRdmaReceiveRequests();
+int32_t _SIDEBAND_FUNC GetSidebandConnectionAddress(::SidebandStrategy strategy, char address[1024]);
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+int32_t _SIDEBAND_FUNC SidebandData_Write(int64_t sidebandToken, const uint8_t* bytes, int64_t byteCount);
+int32_t _SIDEBAND_FUNC SidebandData_Read(int64_t sidebandToken, uint8_t* bytes, int64_t bufferSize, int64_t* numBytesRead);
+int32_t _SIDEBAND_FUNC SidebandData_WriteLengthPrefixed(int64_t sidebandToken, const uint8_t* bytes, int64_t byteCount);
+int32_t _SIDEBAND_FUNC SidebandData_ReadFromLengthPrefixed(int64_t sidebandToken, uint8_t* bytes, int64_t bufferSize, int64_t* numBytesRead);
+int32_t _SIDEBAND_FUNC SidebandData_ReadLengthPrefix(int64_t sidebandToken, int64_t* length);
+int32_t _SIDEBAND_FUNC SidebandData_SupportsDirectReadWrite(int64_t sidebandToken);
+int32_t _SIDEBAND_FUNC SidebandData_BeginDirectRead(int64_t sidebandToken, int64_t byteCount, const uint8_t** buffer);
+int32_t _SIDEBAND_FUNC SidebandData_BeginDirectReadLengthPrefixed(int64_t sidebandToken, int64_t* bufferSize, const uint8_t** buffer);
+int32_t _SIDEBAND_FUNC SidebandData_FinishDirectRead(int64_t sidebandToken);
+int32_t _SIDEBAND_FUNC SidebandData_BeginDirectWrite(int64_t sidebandToken, uint8_t** buffer);
+int32_t _SIDEBAND_FUNC SidebandData_FinishDirectWrite(int64_t sidebandToken, int64_t byteCount);
+int32_t _SIDEBAND_FUNC SidebandData_SerializeBuffer(int64_t sidebandToken, uint8_t** buffer);
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+int32_t _SIDEBAND_FUNC QueueSidebandConnection(::SidebandStrategy strategy, const char* id, bool waitForReader, bool waitForWriter, int64_t bufferSize);
+
+#ifdef __cplusplus
+   }
+#endif
---
+++ sideband/sideband-build/sideband_grpc.h
@@ -0,0 +1,78 @@
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+#pragma once
+// TODO: Evaluate the warning for data loss between int64_t to int and size_t to int
+#pragma warning(disable : 4244)
+#pragma warning(disable : 4267)
+
+#include <cstdint>
+#include <data_moniker.pb.h>
+#include "sideband_data.h"
+#include "sideband_internal.h"
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+inline int64_t InitMonikerSidebandData(const ni::data_monikers::BeginMonikerSidebandStreamResponse& initResponse)
+{
+    int64_t token;
+    InitClientSidebandData(initResponse.connection_url().c_str(), (::SidebandStrategy)initResponse.strategy(), initResponse.sideband_identifier().c_str(), initResponse.buffer_size(), &token);
+    return token;
+}
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+inline int64_t InitClientSidebandData(const ni::data_monikers::BeginMonikerSidebandStreamResponse& response)
+{
+    int64_t token;
+    InitClientSidebandData(response.connection_url().c_str(), (::SidebandStrategy)response.strategy(), response.sideband_identifier().c_str(), response.buffer_size(), &token);
+    return token;
+}
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+inline bool ReadSidebandMessage(int64_t dataToken, google::protobuf::MessageLite* message)
+{
+    bool success = false;
+    if (SidebandData_SupportsDirectReadWrite(dataToken) == 1)
+    {
+        int64_t bufferSize = 0;
+        const uint8_t* buffer = nullptr;
+        SidebandData_BeginDirectReadLengthPrefixed(dataToken, &bufferSize, &buffer);
+        success = message->ParseFromArray(buffer, bufferSize);
+        SidebandData_FinishDirectRead(dataToken);
+    }
+    else
+    {
+        int64_t bufferSize = 0;
+        SidebandData_ReadLengthPrefix(dataToken, &bufferSize);
+        int64_t bytesRead = 0;
+        uint8_t* buffer = nullptr;
+        SidebandData_SerializeBuffer(dataToken, &buffer);
+        SidebandData_ReadFromLengthPrefixed(dataToken, buffer, bufferSize, &bytesRead);
+        success = message->ParseFromArray(buffer, bufferSize);
+        assert(success);
+    }
+    return success;
+}
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+inline int64_t WriteSidebandMessage(int64_t dataToken, const google::protobuf::MessageLite& message)
+{
+    auto byteSize = message.ByteSizeLong();
+    if (SidebandData_SupportsDirectReadWrite(dataToken) == 1)
+    {
+        uint8_t* buffer = nullptr;
+        SidebandData_BeginDirectWrite(dataToken, &buffer);
+        message.SerializeToArray(buffer, byteSize);
+        SidebandData_FinishDirectWrite(dataToken, byteSize);
+    }
+    else
+    {
+        uint8_t* buffer = nullptr;
+        SidebandData_SerializeBuffer(dataToken, &buffer);
+        message.SerializeToArray(buffer, byteSize);
+        SidebandData_WriteLengthPrefixed(dataToken, buffer, byteSize);
+    }
+    return byteSize;
+}
